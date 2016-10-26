0f31a1ac5c1a27e8eacf491216dcb9cc083354ba
Sharynne Azhar <sharynneazhar@gmail.com>
2016-10-26 17:06:26 -0500

    with fixtures commit

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,3 +1,6 @@
+// This defines a starting set of data to be loaded if the app is loaded with an empty db.
+import './fixtures.js';
+
 import '../../api/buildings/methods.js';
 import '../../api/buildings/publications.js';
 
