5a2ed4bae576f4b35d36c243f61af9ff700fa8b6
Ashli Mosiman <amosiman93@gmail.com>
2016-12-06 16:26:47 -0600

    working on search page

--- imports/ui/pages/searchResults/index.js
+++ imports/ui/pages/searchResults/index.js
@@ -3,20 +3,73 @@ import React, {
   PropTypes
 } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { createContainer } from 'meteor/react-meteor-data';
+import { browserHistory } from 'react-router'
+import Buildings, { findBuildingByName, } from '../../../api/buildings';
+import Rooms, { findRoomByNumber, } from '../../../api/rooms';
 import ui from '../../components';
 
 class SearchResults extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+     searchInput: '',
+    };
+    Meteor.subscribe('buildings');
+    this.handleInput = this.handleInput.bind(this);
+    this.findBuilding = this.findBuilding.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+     searchInput: e.target.value,
+    });
+  }
+
+  findBuilding(e) {
+    e.preventDefault();
+    let query = Buildings.find({
+     name: {
+        $regex: this.state.searchInput,
+        $options: 'i'
+     },
+    }).fetch();
+    let url = `/building/${query[0].name}`;
+    this.props.history.push(url);
   }
 
   render() {
-    return (
-      <div className="container">
-        Hello world!
-      </div>
-    );
+       return (
+        <div>
+          <div className="find-section">
+            <div className="find-section-title">
+              Find a building
+            </div>
+            <form className="searchForm" onSubmit={this.search}>
+              <ui.SearchInput
+                type="text"
+                placeholder="building name"
+                onChange={this.handleInput}
+              />
+              <ui.Button
+                style="btn-lavender btn btn-xl"
+                type="submit"
+                text="Search"
+              />
+            </form>
+          </div>
+          <div className="search-results-list">
+               <div className="theResults">
+                    <div className="results-heading">
+                         <div className="search-results-title">Search Results</div>
+                         <div className="search-results-found">Showing 1-3 of 3 Found</div>
+                    </div>
+                    <div className="actual-results">
+
+                    </div>
+               </div>
+          </div>
+        </div>
+      );
   }
 }
 
